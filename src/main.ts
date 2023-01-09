725cf208f2da36a54604b22c2bc7097666eef9a2
lehuuhieu-0805 <01643815296h@gmail.com>
2023-01-09 11:15:08 +0700

    log request

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,14 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { Logger } from '@nestjs/common';
+import * as morgan from 'morgan';
 
 async function bootstrap() {
   const logger = new Logger();
 
   const app = await NestFactory.create(AppModule);
+
+  app.use(morgan('short'));
 
   const port = process.env.PORT;
   await app.listen(port);
