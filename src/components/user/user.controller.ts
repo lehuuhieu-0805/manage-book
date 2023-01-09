112e2b88a87761a4281ae0aca0612c74a6264072
lehuuhieu-0805 <01643815296h@gmail.com>
2023-01-09 14:43:10 +0700

    feat(User): add swagger

--- src/components/user/user.controller.ts
+++ src/components/user/user.controller.ts
@@ -1,11 +1,13 @@
 import { Body, Controller, Get, Inject, Param, Post } from '@nestjs/common';
 import { Patch } from '@nestjs/common/decorators';
+import { ApiResponse, ApiTags } from '@nestjs/swagger';
 import { CreateUserDto } from './dto/createUser.dto';
 import { UpdateUserDto } from './dto/updateUser.dto';
 import { IUserService, USER_SERVICE } from './interface/user.service.interface';
 import { User } from './user.entity';
 
-@Controller('user')
+@Controller('users')
+@ApiTags('User')
 export class UserController {
   // We request Nest inject the provider into our controller.
   // Now when we want to inject USER SERVICE we can use the @Inject decorator and provide our token so that the dependency injector knows what implementation to inject
@@ -15,16 +17,19 @@ export class UserController {
   ) {}
 
   @Get(':id')
+  @ApiResponse({ status: 200, description: 'Find user by id successfully' })
   async findById(@Param('id') id: string): Promise<User> {
     return await this.userService.findById(id);
   }
 
   @Post()
+  @ApiResponse({ status: 201, description: 'Create a user successfully' })
   async create(@Body() userDto: CreateUserDto): Promise<User> {
     return await this.userService.create(userDto);
   }
 
   @Patch(':id')
+  @ApiResponse({ status: 200, description: 'Update a user successfully' })
   async update(
     @Param('id') id: string,
     @Body() userDto: UpdateUserDto,
--- src/main.ts
+++ src/main.ts
@@ -12,10 +12,9 @@ async function bootstrap() {
   app.use(morgan('short'));
 
   const config = new DocumentBuilder()
-    .setTitle('Cats example')
-    .setDescription('The cats API description')
+    .setTitle('NestJs Manage Book')
+    .setDescription('API Manage Book')
     .setVersion('1.0')
-    .addTag('cats')
     .build();
   const document = SwaggerModule.createDocument(app, config);
   SwaggerModule.setup('api', app, document);
